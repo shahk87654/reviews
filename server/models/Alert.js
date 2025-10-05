0d1faf6a03ebc74a6b714435cfad6d010f001e66
Shahnawaz2003555 <shahnawaz.khan@gno.com.pk>
2025-10-05 17:40:28 +0500

    Fix deployment by adding missing Alert model and update MONGO_URI with database name

---
+++ server/models/Alert.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const AlertSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['info', 'warning', 'error', 'success'],
+    default: 'info'
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Alert', AlertSchema);
